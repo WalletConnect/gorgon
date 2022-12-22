0eb004ecd507a206ae047dabc9c10f44caef5b8c
Xavier Basty <xavier@walletconnect.com>
2022-12-22 11:19:10 +0100

    feat: add city and regions to geo data (#4)

--- src/geoip.rs
+++ src/geoip.rs
@@ -11,6 +11,8 @@ pub struct GeoIpReader {
 
 #[derive(Debug, Clone)]
 pub struct AnalyticsGeoData {
+    pub city: Option<Arc<str>>,
+    pub region: Option<Vec<String>>,
     pub country: Option<Arc<str>>,
     pub continent: Option<Arc<str>>,
 }
@@ -46,10 +48,39 @@ impl GeoIpReader {
             .lookup::<Country>(addr)
             .ok()
             .map(|country| AnalyticsGeoData {
+                city: None,
+                region: None,
                 country: country
                     .country
                     .and_then(|country| country.iso_code.map(Into::into)),
                 continent: country
+                    .continent
+                    .and_then(|continent| continent.code.map(Into::into)),
+            })
+    }
+
+    pub fn lookup_geo_data_with_city(&self, addr: IpAddr) -> Option<AnalyticsGeoData> {
+        use maxminddb::geoip2::City;
+
+        self.reader
+            .as_ref()?
+            .lookup::<City>(addr)
+            .ok()
+            .map(|city| AnalyticsGeoData {
+                city: city
+                    .city
+                    .and_then(|city| city.names)
+                    .and_then(|city_names| city_names.get("en").copied().map(Into::into)),
+                region: city.subdivisions.map(|divs| {
+                    divs.into_iter()
+                        .filter_map(|div| div.iso_code)
+                        .map(Into::into)
+                        .collect()
+                }),
+                country: city
+                    .country
+                    .and_then(|country| country.iso_code.map(Into::into)),
+                continent: city
                     .continent
                     .and_then(|continent| continent.code.map(Into::into)),
             })
